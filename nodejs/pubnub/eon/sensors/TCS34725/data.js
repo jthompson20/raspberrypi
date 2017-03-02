15be65bd4e1bcc66c85214b2fce9bdd1fedc35c4
Matt Thompson <matt.thompson@stack.com>
2017-03-01 22:59:37 -0500

    testing TCS34725

---
+++ nodejs/pubnub/eon/sensors/TCS34725/data.js
@@ -0,0 +1,18 @@
+var PubNub = require('pubnub');
+
+var pubnub = new PubNub({
+    publishKey   : "pub-c-561a7378-fa06-4c50-a331-5c0056d0163c",
+    subscribeKey : "sub-c-17b7db8a-3915-11e4-9868-02ee2ddab7fe"
+});
+
+var number = 100;
+function publish() {
+    number += (Math.random() - 0.5);
+    var message = { eon: {"value" : number} };
+    console.log("sending",message);
+    pubnub.publish({
+        channel   : 'example-eon',
+        message   : message,
+    });
+}
+setInterval(publish,3*1000); //every three seconds
