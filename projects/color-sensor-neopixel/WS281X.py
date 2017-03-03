633081ef2cf8ced232352055591b0dbdbc7f8ac7
Matt Thompson <matt.thompson@stack.com>
2017-03-03 13:34:50 -0500

    debugging color sensor project

--- projects/color-sensor-neopixel/WS281X.py
+++ projects/color-sensor-neopixel/WS281X.py
@@ -8,7 +8,7 @@ LED_COUNT      = 1         	# How many LEDs to light.
 LED_FREQ_HZ    = 800000     # Frequency of the LED signal.  Should be 800khz or 400khz.
 LED_DMA_NUM    = 5          # DMA channel to use, can be 0-14.
 LED_GPIO       = 18         # GPIO connected to the LED signal line.  Must support PWM!
-LED_BRIGHTNESS = 255        # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 50        # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = 0          # Set to 1 to invert the LED signal, good if using NPN
 							# transistor as a 3.3V->5V level converter.  Keep at 0
 							# for a normal/non-inverted signal.
