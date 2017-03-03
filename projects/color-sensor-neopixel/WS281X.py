96043d2b6dadcd04e480cd52e1544335b1776070
Matt Thompson <matt.thompson@stack.com>
2017-03-03 12:14:44 -0500

    debugging color sensor project

--- projects/color-sensor-neopixel/WS281X.py
+++ projects/color-sensor-neopixel/WS281X.py
@@ -36,7 +36,7 @@ class WS281X(object):
 
 		# Initialize all channels to off
 		for channum in range(2):
-		    channel = ws.ws2811_channel_get(leds, channum)
+		    channel = ws.ws2811_channel_get(self.leds, channum)
 		    ws.ws2811_channel_t_count_set(channel, 0)
 		    ws.ws2811_channel_t_gpionum_set(channel, 0)
 		    ws.ws2811_channel_t_invert_set(channel, 0)
