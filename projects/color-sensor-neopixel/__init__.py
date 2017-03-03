964fd23a4fde63518dd5c330f1e58a9ba3241c10
Matt Thompson <matt.thompson@stack.com>
2017-03-03 18:08:50 -0500

    updated color-sensor-neopixel project

--- projects/color-sensor-neopixel/__init__.py
+++ projects/color-sensor-neopixel/__init__.py
@@ -34,7 +34,7 @@ if ver == 0x44:
 			print crgb
 
 			# update RGB
-			rgb.update(red,green,blue)
+			rgb.update(red,green,blue,clear)
 			
 			# RGBC ADC wait time (see datasheet: https://cdn-shop.adafruit.com/datasheets/TCS34725.pdf)
 			time.sleep(.44)
