cad66d3268b84dd620beb6df40151fc1bd2bca54
Matt Thompson <matt.thompson@stack.com>
2017-03-04 15:34:30 -0500

    updated python pubnub example

--- pubnub/python/hello-world.py
+++ pubnub/python/hello-world.py
@@ -1,3 +1,4 @@
+import sys
 from pubnub.callbacks import SubscribeCallback
 from pubnub.enums import PNStatusCategory
 from pubnub.pnconfiguration import PNConfiguration
@@ -14,8 +15,12 @@ pubnub = PubNub(pnconfig)
 def my_publish_callback(envelope, status):
     # Check whether request successfully completed or not
     if not status.is_error():
+        print("successfully published")
         pass  # Message successfully published to specified channel.
     else:
+        print("error")
+        print(status)
+        print('')
         pass  # Handle message publish error. Check 'category' property to find out possible issue
         # because of which request did fail.
         # Request can be resent using: [status retry];
@@ -23,10 +28,15 @@ def my_publish_callback(envelope, status):
  
 class MySubscribeCallback(SubscribeCallback):
     def presence(self, pubnub, presence):
+        print('incoming presence data')
+        print(pubnub)
+        print(presence)
+        print('')
         pass  # handle incoming presence data
  
     def status(self, pubnub, status):
         if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+            print('connectivity lost')
             pass  # This event happens when radio / connectivity is lost
  
         elif status.category == PNStatusCategory.PNConnectedCategory:
@@ -34,20 +44,30 @@ class MySubscribeCallback(SubscribeCallback):
             # Or just use the connected event to confirm you are subscribed for
             # UI / internal notifications, etc
             pubnub.publish().channel("awesomeChannel").message("hello!!").async(my_publish_callback)
+            print('published')
         elif status.category == PNStatusCategory.PNReconnectedCategory:
+            print('connectivity lost again')
             pass
             # Happens as part of our regular operation. This event happens when
             # radio / connectivity is lost, then regained.
         elif status.category == PNStatusCategory.PNDecryptionErrorCategory:
+            print('encryption error')
             pass
             # Handle message decryption error. Probably client configured to
             # encrypt messages and on live data feed it received plain text.
  
     def message(self, pubnub, message):
+        print('new message')
+        print(message)
+        print('')
         pass  # Handle new message stored in message.message
  
  
-pubnub.add_listener(MySubscribeCallback())
-pubnub.subscribe().channels('awesomeChannel').execute()
-
+try:
+    pubnub.add_listener(MySubscribeCallback())
+    pubnub.subscribe().channels('awesomeChannel').execute()
+except KeyboardInterrupt:
+    sys.exit()
+finally:
+    sys.exit()
 
