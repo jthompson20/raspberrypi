18a3e81ec42230f1ba39431d08cf2530db1db22f
Matt Thompson <matt.thompson@stack.com>
2017-03-07 21:58:53 -0500

    tkinter gui example

---
+++ gui/tkinter/grid.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import time
+
+master 	= Tk()
+
+Label(master, text="First").grid(row=0)
+Label(master, text="Second").grid(row=1)
+
+e1 = Entry(master)
+e2 = Entry(master)
+
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+
+time.sleep(60)
