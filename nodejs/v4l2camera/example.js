12a73ad8a90e95480bd94bfbcd832122da54158a
Matt Thompson <matt.thompson@stack.com>
2017-03-08 17:45:49 -0500

    updated v4l2 example

--- nodejs/v4l2camera/example.js
+++ nodejs/v4l2camera/example.js
@@ -1,13 +1,20 @@
 var v4l2camera = require("v4l2camera");
 var cam = new v4l2camera.Camera("/dev/video0");
+
+/*
 if (cam.configGet().formatName !== "MJPG") {
   console.log("NOTICE: MJPG camera required");
   process.exit(1);
 }
+*/
+
 cam.start();
 cam.capture(function (success) {
-  var frame = cam.frameRaw();
-  require("fs").createWriteStream("result.jpg").end(Buffer(frame));
-  cam.stop();
+	console.log('capturing..');
+	var yuyv 	= cam.toYUYV();
+	console.log(yuyv);
+	var frame 	= cam.frameRaw();
+	console.log(frame);
+	//require("fs").createWriteStream("result.jpg").end(Buffer(frame));
+	cam.stop();
 });
-console.log('complete');
