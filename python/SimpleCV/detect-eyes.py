07b4dc48dde389ed5e840d4b121022369882ff94
Matt Thompson <matt.thompson@stack.com>
2017-03-08 13:04:21 -0500

    added simplecv detect eyes script

--- python/SimpleCV/detect-eyes.py
+++ python/SimpleCV/detect-eyes.py
@@ -1,14 +1,16 @@
 from SimpleCV import Display, Image, Camera
 
+#initialize the camera
+cam 	= Camera()
 display = Display()
 
 # Loop until window is closed
 while not display.isDone():
 
-
-
+	# Get image, flip it so it looks mirrored, scale to speed things up
+    img = cam.getImage().flipHorizontal().scale(0.5)
 	# Look for a face
-	faces = img.findHaarFeatures('face')
+	faces = img.findHaarFeatures('eye.xml')
 	if faces is not None:
 	# Draw a box around the face
 	faces.draw()
