acd78969a2dacf9ca71d8d2da92b1ec06a6fef82
Matt Thompson <matt.thompson@stack.com>
2017-03-08 11:50:29 -0500

    added simplecv image processing example

---
+++ python/SimpleCV/image-processing.py
@@ -0,0 +1,22 @@
+from SimpleCV import Camera
+# Initialize the camera
+cam = Camera()
+# Loop to continuously get images
+while True:
+    # Get Image from camera
+    img = cam.getImage()
+    img = img.edges()    
+	img.show()    
+	time.sleep(5)
+
+	# show binary
+	img = img.binarize()  
+	img.show()  
+	time.sleep(5)    
+
+	# find blobs
+	img = img.findBlobs()  
+	for blob in blobs:  
+	    blob.draw()  
+	img.show()  
+	time.sleep(5) 
