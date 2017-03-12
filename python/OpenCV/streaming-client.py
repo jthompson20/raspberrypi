fbd78f3d373ae0368922fd556425231f42009166
Matt Thompson <matt.thompson@stack.com>
2017-03-12 13:15:27 -0400

    added opencv client and server examples

--- python/OpenCV/streaming-client.py
+++ python/OpenCV/streaming-client.py
@@ -10,4 +10,4 @@ clientsocket.connect(('localhost',8081))
 while True:
     ret,frame=cap.read()
     data = pickle.dumps(frame) ### new code
-    clientsocket.sendall(struct.pack("H", len(data))+data) ### new code
+    clientsocket.sendall(struct.pack("L", len(data))+data) ### new code
